7c1d138d5e55c4a96ae2dc425f168d9eb92c55fe
Adrian <AdrianForde02@gmail.com>
2020-05-17 05:11:39 -0300

    Added a parallax effect to header graphics on scroll

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const nameSpans = document.querySelectorAll('.name span');
 //Set highlight color for span letters
 var textHighlightColor;
 
+//Adding onclick to hamburger to operate navbar
+document.querySelector('.hamburger').addEventListener('click', () => {
+  document.querySelector('.nav-wrapper ul').classList.toggle('nav-open');
+});
+
 //Decide which highlight color to use when page loads
 window.onload = () => {
   const vw = Math.max(
@@ -29,9 +34,9 @@ window.addEventListener('resize', () => {
   } else if (textHighlightColor !== '#f15f79') textHighlightColor = '#f15f79';
 });
 
-// function getRandomArbitrary(min, max) {
-//   return Math.floor(Math.random() * (max - min) + min);
-// }
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
 //Loops through all span letters and change their color to the highlight color and
 //change them back after a period of time. Do this continuously
@@ -53,3 +58,23 @@ setInterval(() => {
     }, 500 * index + 1);
   });
 }, 500 * nameSpans.length);
+
+//Paralax Effect
+window.addEventListener('scroll', (e) => {
+  const headerGraphics = document.querySelectorAll('#scroll');
+
+  var index = 0;
+  var length = headerGraphics.length;
+
+  for (index; index < length; index++) {
+    var pos = window.pageYOffset * headerGraphics[index].dataset.parallaxRate;
+
+    headerGraphics[index].style.transform = `translate3d(0px, ${pos}px, 0px)`;
+  }
+
+  // nameSpans.forEach((span) => {
+  //   var spanRate = scrolled * getRandomArbitrary(0.5, 2);
+  //   console.log(spanRate);
+  //   span.style.transform = `translate3d(0px, ${rate}px, 0px)`;
+  // });
+});
