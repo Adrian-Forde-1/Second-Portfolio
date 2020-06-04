25f4af2b6f3319662512003d69d16f4dc16b8e4e
Adrian <AdrianForde02@gmail.com>
2020-06-04 08:07:55 -0300

    Added project to homepage animation

--- JS/barba.js
+++ JS/barba.js
@@ -14,6 +14,7 @@ function projectTransition(path) {
   path = path.split('/')[1].split('.')[0];
   if (path === 'cellphonerepair') {
     pageName = document.querySelector('#cellphone-repair-website-name');
+
     pageNamePath = document.querySelectorAll(
       '#cellphone-repair-website-name path'
     );
@@ -21,26 +22,43 @@ function projectTransition(path) {
     pageNamePath.forEach((path) => {
       path.style.animation = `dash 1s ease-out 200ms`;
     });
+  } else if (path === 'restaurantwebsite') {
+    pageName = document.querySelector('#restaurant-website-name');
+
+    pageNamePath = document.querySelectorAll('#restaurant-website-name path');
+
+    pageNamePath.forEach((path) => {
+      path.style.animation = `dash 1s ease-out 200ms`;
+    });
+  } else if (path === 'bugtracker') {
+    pageName = document.querySelector('#bug-tracker-website-name');
+
+    pageNamePath = document.querySelectorAll('#bug-tracker-website-name path');
+
+    pageNamePath.forEach((path) => {
+      path.style.animation = `dash 1s ease-out 200ms`;
+    });
+  } else if (path === 'sortingalgorithmvisualizer') {
+    pageName = document.querySelector(
+      '#sortingalgorithmvisualizer-website-name'
+    );
+
+    pageNamePath = document.querySelectorAll(
+      '#sortingalgorithmvisualizer-website-name path'
+    );
+
+    pageNamePath.forEach((path) => {
+      path.style.animation = `dash 1s ease-out 200ms`;
+    });
+  } else if (path === 'clothingwebsite') {
+    pageName = document.querySelector('#clothing-website-name');
+
+    pageNamePath = document.querySelectorAll('#clothing-website-name path');
+
+    pageNamePath.forEach((path) => {
+      path.style.animation = `dash 1s ease-out 200ms`;
+    });
   }
-  // else if (path === 'bugtracker') {
-  //   pageName = document.querySelector('#bug-tracker-website-name');
-
-  //   pageName.style.animation = `dash 300ms ease-out 0.45`;
-  // } else if (path === 'sortingalgorithmvisualizer') {
-  //   pageName = document.querySelector(
-  //     '#sortingalgorithmvisualizer-website-name'
-  //   );
-
-  //   pageName.style.animation = `dash 300ms ease-out 0.45`;
-  // } else if (path === 'clothingwebsite') {
-  //   pageName = document.querySelector('#clothing-website-name');
-
-  //   pageName.style.animation = `dash 300ms ease-out 0.45`;
-  // } else if (path === 'restaurantwebsite') {
-  //   pageName = document.querySelector('#restaurant-website-name');
-
-  //   pageName.style.animation = `dash 300ms ease-out 0.45`;
-  // }
 
   tl.to('.transition', { duration: 0.4, scaleX: 1 });
   tl.to(pageName, {
@@ -52,15 +70,44 @@ function projectTransition(path) {
     duration: 0.3,
     fill: '#fff',
   });
-
+  tl.to(pageName, {
+    delay: 0.5,
+    opacity: 0,
+    duration: 0.3,
+    ease: 'power1.inOut',
+  });
   tl.to('.transition', {
     duration: 0.5,
     scaleY: 0,
     transformOrigin: 'top',
-    delay: 0.6,
   });
   tl.set('.transition', { clearProps: 'all' });
   tl.set(pageName, { clearProps: 'all' });
+}
+
+function projectPageEnter() {
+  var tl = gsap.timeline();
+}
+
+function homeTransition(path) {
+  console.log('Home transition called');
+  var tl = gsap.timeline();
+
+  tl.set('.transition-2', { display: 'block' });
+  tl.to('.transition-2 ul li', { duration: 0.4, scaleX: 1, stagger: 0.2 });
+}
+
+function homePageEnter() {
+  var tl = gsap.timeline();
+
+  tl.to('.transition-2 ul li', {
+    duration: 0.4,
+    scaleX: 0,
+    stagger: 0.2,
+    direction: 'reverse',
+  });
+  tl.to('.transition-2 ul', { clearProps: 'all' });
+  tl.to('.transition-2 ul li', { clearProps: 'all' });
 }
 
 function removeStyles() {
@@ -68,17 +115,6 @@ function removeStyles() {
     path.style.animation = '';
     path.style.strokeDashoffset = 1;
   });
-}
-function projectPageEnter() {
-  var tl = gsap.timeline();
-  // var pageName = null;
-
-  // else if (path === 'bugtracker') pageName = 'Bug Tracker Website';
-  // else if (path === 'sortingalgorithmvisualizer')
-  //   pageName = 'Sorting Algorithm Visualizer Website';
-  // else if (path === 'clothingwebsite') pageName = 'Clothing Website';
-  // else if (path === 'restaurantwebsite') pageName = 'Restaurant Website';
-  // tl.set(pageName, { clearProps: 'all' });
 }
 
 const delay = (n) => {
@@ -96,15 +132,23 @@ barba.init({
     {
       async leave(data) {
         const done = this.async();
-        sessionStorage.setItem('data', JSON.stringify(data));
-
-        projectTransition(data.next.url.path);
-        await delay(1500);
-        done();
+        if (data.next.url.path.split('/')[1] !== 'index.html') {
+          projectTransition(data.next.url.path);
+          await delay(1500);
+          done();
+        } else {
+          homeTransition();
+          await delay(1500);
+          done();
+        }
       },
 
       async enter(data) {
-        projectPageEnter(data.next.url.path);
+        if (data.next.url.path.split('/')[1] !== 'index.html') {
+          projectPageEnter(data.next.url.path);
+        } else {
+          homePageEnter();
+        }
       },
     },
   ],
