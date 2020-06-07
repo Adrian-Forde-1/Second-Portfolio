c4e160748214b55a870d27f545af7049fade07df
Adrian <AdrianForde02@gmail.com>
2020-06-07 20:02:32 -0300

    Removed header animation

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,3 @@
-//Get all span letters
-var nameSpans = document.querySelectorAll('.name span');
-
-//Set highlight color for span letters
-var textHighlightColor;
-
 //Adding onclick to hamburger to operate navbar
 
 if (document.querySelector('.hamburger')) {
@@ -11,20 +5,6 @@ if (document.querySelector('.hamburger')) {
     document.querySelector('.nav-wrapper ul').classList.toggle('nav-open');
   });
 }
-
-//Decide which highlight color to use when page loads
-window.onload = () => {
-  var vw = Math.max(
-    document.documentElement.clientWidth,
-    window.innerWidth || 0
-  );
-
-  if (vw <= 567) {
-    textHighlightColor = '#64ffda';
-  } else {
-    textHighlightColor = '#f15f79';
-  }
-};
 
 //Decide which highlight color to use when page resizes
 window.addEventListener('resize', () => {
@@ -40,31 +20,6 @@ window.addEventListener('resize', () => {
 function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
-
-//Loops through all span letters and change their color to the highlight color and
-//change them back after a period of time. Do this continuously
-nameSpans.forEach((span, index) => {
-  if (span.innerHTML !== '&nbsp;') {
-    setTimeout(() => {
-      span.style.color = textHighlightColor;
-      setTimeout(() => {
-        span.style.color = 'white';
-      }, 500);
-    }, 500 * index + 1);
-  }
-});
-setInterval(() => {
-  nameSpans.forEach((span, index) => {
-    if (span.innerHTML !== '&nbsp;') {
-      setTimeout(() => {
-        span.style.color = textHighlightColor;
-        setTimeout(() => {
-          span.style.color = 'white';
-        }, 500);
-      }, 500 * index + 1);
-    }
-  });
-}, 500 * nameSpans.length);
 
 //Paralax Effect
 window.addEventListener('scroll', (e) => {
