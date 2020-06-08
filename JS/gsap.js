1445b6e8f018ee967f954f9583e8091c371ccf00
Adrian <AdrianForde02@gmail.com>
2020-06-08 01:21:28 -0300

    Added animations for project page

--- JS/gsap.js
+++ JS/gsap.js
@@ -1,151 +1,140 @@
-gsap.registerPlugin(ScrollTrigger);
+console.log(window.location.pathname);
+if (
+  window.location.pathname === '/' ||
+  window.location.pathname === '/index.html'
+) {
+  gsap.registerPlugin(ScrollTrigger);
 
-//Animate the stroke of the section name
-//Add the fill
-//Animate the decoration's scale
-document.querySelectorAll('section').forEach((section) => {
-  const sectionName = section.querySelector('#section-name');
-  const sectionNameDecoration = section.querySelector(
-    '.section-name-decoration'
-  );
+  //Animate the stroke of the section name
+  //Add the fill
+  //Animate the decoration's scale
+  document.querySelectorAll('section').forEach((section) => {
+    const sectionName = section.querySelector('#section-name');
+    const sectionNameDecoration = section.querySelector(
+      '.section-name-decoration'
+    );
 
-  //Create a timeline with a scrollTrigger
-  var tl = gsap.timeline({
+    //Create a timeline with a scrollTrigger
+    var tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: section,
+        start: 'top center',
+        toggleActions: 'play restart resume reverse',
+      },
+    });
+
+    //Animate the secion name stroke
+    tl.to(sectionName, {
+      duration: 1,
+      strokeDashoffset: 0,
+    });
+    //Animate decoration's scale
+    tl.to(sectionName, {
+      duration: 0.3,
+      fill: '#fff',
+    });
+    tl.to(
+      sectionNameDecoration,
+      {
+        scaleX: 1,
+        duration: 0.4,
+      },
+      '-=0.3'
+    );
+  });
+
+  document.querySelectorAll('.skills-content').forEach((skillContent) => {
+    gsap.to(skillContent, {
+      scrollTrigger: {
+        trigger: skillContent,
+        start: 'top center',
+        toggleActions: 'play restart resume reverse',
+      },
+      right: '-50%',
+      skewX: '-15',
+      skewType: 'simple',
+      duration: 1,
+      ease: 'power4.out',
+    });
+
+    const skillInfo = skillContent.querySelector('.skill-group-information');
+
+    gsap.to(skillInfo, {
+      scrollTrigger: {
+        trigger: skillContent,
+        start: 'top center',
+        toggleActions: 'play restart resume reverse',
+      },
+      skewX: 15,
+      duration: 0.3,
+    });
+  });
+
+  gsap.from('.about-me-graphic-right, .about-me-graphic-left', {
     scrollTrigger: {
-      trigger: section,
+      trigger: '.about-me',
       start: 'top center',
       toggleActions: 'play restart resume reverse',
     },
+    scale: 0,
+    duration: 1,
+    ease: 'back',
   });
 
-  //Animate the secion name stroke
-  tl.to(sectionName, {
-    duration: 1,
-    strokeDashoffset: 0,
-  });
-  //Animate decoration's scale
-  tl.to(sectionName, {
-    duration: 0.3,
-    fill: '#fff',
-  });
-  tl.to(
-    sectionNameDecoration,
-    {
-      scaleX: 1,
+  document.querySelector('.about-me').addEventListener('mouseenter', () => {
+    gsap.to('.about-me-graphic-left, .about-me-graphic-right', {
+      scale: 1.2,
       duration: 0.4,
-    },
-    '-=0.3'
-  );
-});
-
-document.querySelectorAll('.skills-content').forEach((skillContent) => {
-  gsap.to(skillContent, {
-    scrollTrigger: {
-      trigger: skillContent,
-      start: 'top center',
-      toggleActions: 'play restart resume reverse',
-    },
-    right: '-50%',
-    skewX: '-15',
-    skewType: 'simple',
-    duration: 1,
-    ease: 'power4.out',
+    });
+  });
+  document.querySelector('.about-me').addEventListener('mouseleave', () => {
+    gsap.to('.about-me-graphic-left, .about-me-graphic-right', {
+      scale: 1,
+      duration: 0.4,
+    });
   });
 
-  const skillInfo = skillContent.querySelector('.skill-group-information');
-
-  gsap.to(skillInfo, {
+  gsap.to('#frontend-projects #project', {
     scrollTrigger: {
-      trigger: skillContent,
-      start: 'top center',
+      trigger: '#frontend-projects',
+      start: 'top bottom',
       toggleActions: 'play restart resume reverse',
     },
-    skewX: 15,
-    duration: 0.3,
-  });
-});
-
-gsap.from('.about-me-graphic-right, .about-me-graphic-left', {
-  scrollTrigger: {
-    trigger: '.about-me',
-    start: 'top center',
-    toggleActions: 'play restart resume reverse',
-  },
-  scale: 0,
-  duration: 1,
-  ease: 'back',
-});
-
-document.querySelector('.about-me').addEventListener('mouseenter', () => {
-  gsap.to('.about-me-graphic-left, .about-me-graphic-right', {
-    scale: 1.2,
     duration: 0.4,
-  });
-});
-document.querySelector('.about-me').addEventListener('mouseleave', () => {
-  gsap.to('.about-me-graphic-left, .about-me-graphic-right', {
     scale: 1,
-    duration: 0.4,
+    opacity: 1,
+    stagger: 0.2,
   });
-});
 
-gsap.to('#frontend-projects #project', {
-  scrollTrigger: {
-    trigger: '#frontend-projects',
-    start: 'top bottom',
-    toggleActions: 'play restart resume reverse',
-  },
-  duration: 0.4,
-  scale: 1,
-  opacity: 1,
-  stagger: 0.2,
-});
-
-gsap.to('#fullstack-projects #project', {
-  scrollTrigger: {
-    trigger: '#fullstack-projects',
-    start: 'top bottom',
-    toggleActions: 'play restart resume reverse',
-  },
-  duration: 0.4,
-  scale: 1,
-  opacity: 1,
-  stagger: 0.2,
-});
-
-//Set highlight color for span letters
-var textHighlightColor = '#f15f79';
-
-//Decide which highlight color to use when page loads
-window.onload = () => {
-  var vw = Math.max(
-    document.documentElement.clientWidth,
-    window.innerWidth || 0
-  );
-
-  if (vw <= 567) {
-    textHighlightColor = '#64ffda';
-  } else {
-    textHighlightColor = '#f15f79';
-  }
-};
-
-document.querySelectorAll('.name span').forEach((span, index) => {
-  const tl = gsap.timeline();
-  tl.to(span, {
-    duration: 0.5,
-    delay: () => {
-      return 0.5 * index;
+  gsap.to('#fullstack-projects #project', {
+    scrollTrigger: {
+      trigger: '#fullstack-projects',
+      start: 'top bottom',
+      toggleActions: 'play restart resume reverse',
     },
-    color: textHighlightColor,
+    duration: 0.4,
+    scale: 1,
+    opacity: 1,
+    stagger: 0.2,
   });
 
-  tl.to(span, {
-    clearProps: 'all',
-  });
-});
+  //Set highlight color for span letters
+  var textHighlightColor = '#f15f79';
 
-setInterval(() => {
+  //Decide which highlight color to use when page loads
+  window.onload = () => {
+    var vw = Math.max(
+      document.documentElement.clientWidth,
+      window.innerWidth || 0
+    );
+
+    if (vw <= 567) {
+      textHighlightColor = '#64ffda';
+    } else {
+      textHighlightColor = '#f15f79';
+    }
+  };
+
   document.querySelectorAll('.name span').forEach((span, index) => {
     const tl = gsap.timeline();
     tl.to(span, {
@@ -160,4 +149,50 @@ setInterval(() => {
       clearProps: 'all',
     });
   });
-}, document.querySelectorAll('.name span').length * 500);
+
+  setInterval(() => {
+    document.querySelectorAll('.name span').forEach((span, index) => {
+      const tl = gsap.timeline();
+      tl.to(span, {
+        duration: 0.5,
+        delay: () => {
+          return 0.5 * index;
+        },
+        color: textHighlightColor,
+      });
+
+      tl.to(span, {
+        clearProps: 'all',
+      });
+    });
+  }, document.querySelectorAll('.name span').length * 500);
+} else {
+  window.onload = () => {
+    const tl = gsap.timeline();
+    var projectName = document.querySelectorAll(
+      '.individual-project-information h3 span'
+    );
+    console.log(projectName);
+    tl.to('.individual-project-img-container', {
+      duration: 1.25,
+      delay: 0.2,
+      yPercent: 0,
+      opacity: 1,
+      ease: 'power1.inOut',
+    });
+    tl.to(projectName, {
+      top: 0,
+      opacity: 1,
+      stagger: 0.03,
+      ease: 'power3.out',
+    });
+    tl.from('.individual-project-made-with', {
+      duration: 0.5,
+      opacity: 0,
+    });
+    tl.to('.individual-project-description-links-container', {
+      duration: 0.5,
+      opacity: 1,
+    });
+  };
+}
