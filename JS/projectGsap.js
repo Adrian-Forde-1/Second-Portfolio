eee12ec9dda7636b3ce47a420c7aee9067e845db
Adrian <AdrianForde02@gmail.com>
2020-06-22 00:54:48 -0300

    Chnaged some (from) animations to (to) animations

--- JS/projectGsap.js
+++ JS/projectGsap.js
@@ -5,23 +5,23 @@ var projectName = document.querySelectorAll(
 
 console.log(document.querySelector('.individual-project-img-container'));
 
-tl.from('.individual-project-img-container', {
+tl.to('.individual-project-img-container', {
   duration: 1.25,
   delay: 0.2,
-  yPercent: '-50',
-  opacity: 0,
-  ease: 'power1.inOut',
-});
-tl.to(projectName, {
-  top: 0,
+  yPercent: '0',
   opacity: 1,
-  stagger: 0.03,
-  ease: 'power3.out',
-});
-tl.from('.individual-project-made-with', {
-  duration: 0.5,
-  opacity: 0,
-});
+  ease: 'power1.inOut',
+})
+  .to(projectName, {
+    top: 0,
+    opacity: 1,
+    stagger: 0.03,
+    ease: 'power3.out',
+  })
+  .from('.individual-project-made-with', {
+    duration: 0.5,
+    opacity: 0,
+  });
 tl.to('.individual-project-description-links-container', {
   duration: 0.5,
   opacity: 1,
